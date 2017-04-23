e82b8b96c97d7e01b77efa86a789e3b7aaed0a1d
Franco Reyes <freyes1988@gmail.com>
2017-04-23 16:46:31 -0500

    Calculate the total population and house seats

--- main.go
+++ main.go
@@ -16,16 +16,27 @@ type State struct {
 func main() {
 	fileName := "data/states.json"
 	fmt.Println("vim-go")
-
 	raw, err := ioutil.ReadFile(fileName)
 
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-
 	var states []State
 	json.Unmarshal(raw, &states)
 
-	fmt.Println(states[0].Name)
+	var totalPopulation int
+	var totalHouseSeats int
+
+	for _, state := range states {
+		totalPopulation += state.Population
+		totalHouseSeats += state.Seats["house"]
+	}
+
+	fmt.Printf("Total population: %d \n", totalPopulation)
+	fmt.Printf("Total house seats: %d \n", totalHouseSeats)
+}
+
+func CalculateSeatTotal(totalPopulation int, peoplePerSeat int, seatLimit int) {
+
 }
