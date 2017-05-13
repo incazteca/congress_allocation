a8737fb812d0fa2350ae6c2fc46ffdf031642093
Franco Reyes <freyes1988@gmail.com>
2017-05-13 17:03:18 -0500

    Updated tests for calculating seats

--- main_test.go
+++ main_test.go
@@ -14,5 +14,28 @@ type calculateSeatTestCase struct {
 func TestCalculateSeats(t *testing.T) {
 	testCases := []calculateSeatTestCase{
 		calculateSeatTestCase{10, 100, 10, 10},
+		calculateSeatTestCase{10, 1000, 10, 10},
+		calculateSeatTestCase{8, 80, 10, 10},
+		calculateSeatTestCase{8, 80, 10, 10},
+		calculateSeatTestCase{0, 0, 10, 10},
+		calculateSeatTestCase{5, 100, 20, 10},
+		calculateSeatTestCase{3, 100, 20, 3},
+		calculateSeatTestCase{3, 100, 20, 3},
+	}
+
+	for _, testCase := range testCases {
+		result := CalculateSeats(
+			testCase.Population,
+			testCase.PeoplePerSeat,
+			testCase.SeatLimit,
+		)
+
+		if testCase.expectedSeats != result {
+			t.Errorf(
+				"Results don't match, expected: %d, received: %d",
+				testCase.expectedSeats,
+				result,
+			)
+		}
 	}
 }
