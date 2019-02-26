60a4c9730daa15cc03bd8bce29df7961af8af4b8
Franco Reyes <freyes1988@gmail.com>
2019-02-26 00:09:17 -0600

    WIP of founding allocation strategy

--- strategy/founder_amendment.go
+++ strategy/founder_amendment.go
@@ -4,17 +4,31 @@ import (
 	"github.com/incazteca/congress_allocation/state"
 )
 
+const senateSeatsPerState = 2
+
 // CalculateSeats calculate the seats for with the new strategy
+// based on estimated population
 func CalculateSeats(states []state.State) strategyResult {
 	return newStrategySummary(states)
 }
 
 func newStrategySummary(states []state.State) strategySummary {
-	var totalPopulation int
+	var totalPopulationEst int
 	var totalHouseSeats int
 
 	for _, state := range states {
-		totalPopulation += state.PopulationEst
+		totalPopulationEst += state.PopulationEst
+		totalHouseSeats += calculateHouseSeatsForState(state)
+		totalSenateSeats += senateSeatsPerState
 	}
-	return strategySummary{}
+
+	return strategySummary{
+		totalPopulation:  totalPopulationEst,
+		totalHouseSeats:  totalHouseSeats,
+		totalSenateSeats: totalSenateSeats,
+	}
+}
+
+func calculateHouseSeatsForState(currentState state) {
+
 }
