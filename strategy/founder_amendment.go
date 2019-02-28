6de969e8cba48fc853b002302668849da9742fd3
Franco Reyes <freyes1988@gmail.com>
2019-02-28 01:09:20 -0600

    WIP of founder amendment

--- strategy/founder_amendment.go
+++ strategy/founder_amendment.go
@@ -2,24 +2,45 @@ package strategy
 
 import (
 	"github.com/incazteca/congress_allocation/state"
+	"sort"
 )
 
+const strategyName = "Founder Amendment"
 const senateSeatsPerState = 2
+
+const initialPeoplePerSeat = 40000
+const seatStep = 100
+const AdditionalPeoplePerSeat = 10000
 
 // CalculateSeats calculate the seats for with the new strategy
 // based on estimated population
-func CalculateSeats(states []state.State) strategyResult {
-	return newStrategySummary(states)
+func Allocate(states []state.State) strategyResult {
+	summary := newStrategySummary(states)
+
+	sort.Slice(states, func(i, j int) bool {
+		return states[i].PopulationEst < states[j].PopulationEst
+	})
+
+	return strategyResult{
+		Name:    strategyName,
+		States:  states, // Actually work allocations here
+		Summary: summary,
+	}
 }
 
 func newStrategySummary(states []state.State) strategySummary {
 	var totalPopulationEst int
 	var totalHouseSeats int
+	var totalSenateSeats int
 
 	for _, state := range states {
 		totalPopulationEst += state.PopulationEst
-		totalHouseSeats += calculateHouseSeatsForState(state)
 		totalSenateSeats += senateSeatsPerState
+		totalHouseSeats += calculateHouseSeatsForState(state)
+	}
+
+	for _, state := range states {
+		totalHouseSeats += calculateHouseSeatsForState(state)
 	}
 
 	return strategySummary{
@@ -29,6 +50,6 @@ func newStrategySummary(states []state.State) strategySummary {
 	}
 }
 
-func calculateHouseSeatsForState(currentState state) {
-
+func calculateHouseSeatsForState(currentState state.State) int {
+	return len(currentState.Name)
 }
